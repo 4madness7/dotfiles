92c3c215b28c7243a9d2ae93c7d68448def11716
Marco <marcomirizzi4@gmail.com>
2025-01-02 10:44:32 +0100

    added tpl for managing laptop battery

--- madnix/nixos/laptop/configuration.nix
+++ madnix/nixos/laptop/configuration.nix
@@ -59,6 +59,28 @@
     variant = "";
   };
 
+  # Battery
+  services.power-profiles-daemon.enable = false;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+
+      # CPU_MIN_PERF_ON_AC = 0;
+      # CPU_MAX_PERF_ON_AC = 100;
+      # CPU_MIN_PERF_ON_BAT = 0;
+      # CPU_MAX_PERF_ON_BAT = 20;
+
+      # Optional helps save long term battery health
+      # START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+      # STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
+    };
+  };
+
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -118,7 +140,7 @@
 
   programs.gamemode.enable = true;
 
-  programs.nix-ld.enable = true;
+  # programs.nix-ld.enable = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
